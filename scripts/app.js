711b27802c791929c2d0c1daed5026c6eaf5c30c
Nathen Watters <wattersnathen@gmail.com>
2015-04-01 20:32:10 -0600

    flag to indicate when Instagram request is complete. form submit handler that setups the ajax request to the Instagram API

--- scripts/app.js
+++ scripts/app.js
@@ -9,5 +9,32 @@ angular.module('InstagramSearch', ['ngAnimate'])
     var url = 'https://api.instagram.com/v1/tags/%tag%/media/recent', //?count=20
           clientId = '079f0f1b37da4231b24e5331362c4a1a';
 
+    $scope.searchComplete = false;
+
+    $scope.submit = function() {
+        if ($scope.instragramSearchForm.$valid) {
+            $http({
+                method: 'JSONP',
+                url: url.replace("%tag%", $scope.query),
+                params: {
+                    client_id: clientId,
+                    callback: 'JSON_CALLBACK',
+                }
+            })
+            .success(function(data, status, header, config) {
+                $scope.results = data.data;
+                $log.info(data);
+
+                $scope.searchComplete = true;
+            })
+            .error(function(data, status, header, config) {
+                $log.warn("Failure to retrieve images from Instagram");
+            });
+        } else {
+            $log.warn("Form submission was invalid");
+        }
+    };
+
+    $scope.results = [];
 
 }]);
