b44896be64443ca71d2d4f0d194f7b810edc3385
Nathen Watters <wattersnathen@gmail.com>
2015-04-01 20:29:32 -0600

    instagram end point url and client id

--- scripts/app.js
+++ scripts/app.js
@@ -6,5 +6,8 @@ angular.module('InstagramSearch', ['ngAnimate'])
 
 .controller('searchCtrl', ['$scope', '$http', '$log', function($scope, $http, $log) {
     
+    var url = 'https://api.instagram.com/v1/tags/%tag%/media/recent', //?count=20
+          clientId = '079f0f1b37da4231b24e5331362c4a1a';
+
 
 }]);
